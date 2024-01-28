76a4af581b310107ab82828402a58423317ce061
XenoverseUp <xenoverseup@gmail.com>
2024-01-29 00:56:04 +0300

    let there be code

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen flex justify-center items-center bg-black">
+      <Button>Click Me</Button>
+    </main>
+  )
+}
